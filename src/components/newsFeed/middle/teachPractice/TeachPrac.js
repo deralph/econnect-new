6028f4b82bf041325235d7d9a7785e4ca40d03f4
deralph <jraphael441@gmail.com>
2022-10-01 03:49:02 -0700

    finished teaching practice page

--- src/components/newsFeed/middle/teachPractice/TeachPrac.js
+++ src/components/newsFeed/middle/teachPractice/TeachPrac.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import styles from "../../../../styles/newsFeed/teachprac.module.css";
+import style from "../../../../styles/newsFeed/index.module.css";
+import style1 from "../../../../styles/newsFeed/acad.module.css";
 
 const TeachPrac = () => {
   return (
-    <section>
-      <h2>
+    <section className={`${style.center} ${style1.center}`}>
+      <h2 className={style1.head}>
         <FaArrowLeft
           className={styles.icon}
           style={{ color: "#4063D6", marginRight: "10px" }}
@@ -13,10 +15,19 @@ const TeachPrac = () => {
         Teaching Practice{" "}
       </h2>
       <div className={styles.info}>
-        <h3>Faculty of Education Students alone</h3>
-        <p>Check your Teaching Practice Postings</p>
+        <div>
+          <h3>Faculty of Education Students alone</h3>
+          <p>Check your Teaching Practice Postings</p>
+        </div>
       </div>
-      <img src="/rectangle 63.png" alt="school building pics" />
+      <div className={styles.posting}>
+        <p>Matric Number:</p>
+        <form>
+          <input type="text" placeholder="170404110" />
+          <button>View Posting</button>
+        </form>
+        <div className={styles.box}>Your Posting :</div>
+      </div>
     </section>
   );
 };
