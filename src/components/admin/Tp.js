7a6854e3b00c4817e805c398c7dfb18dacb4ef3c
deralph <jraphael441@gmail.com>
2022-10-25 23:03:16 -0700

    did the tp page

--- src/components/admin/Tp.js
+++ src/components/admin/Tp.js
@@ -2,20 +2,32 @@ import React from "react";
 import BackArrow from "../newsFeed/BackArrow";
 
 import styles from "../../styles/admin/tp.module.css";
+import { FaFile } from "react-icons/fa";
 const Tp = () => {
   return (
     <section className="edit">
       <BackArrow noArr heading={"Upload Teaching Practice"} />
       <article className={styles.article}>
-        <div>
+        <div className={styles.image}>
           <img src="/teachingPractice.png" alt="teaching Practice" />
           <p>ONLY FOR FACULTY OF EDUCATION</p>
         </div>
-        <div>
-          <p></p>
-          <p></p>
-          <div></div>
+        <div className={styles.info}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quis
+            commodi quas nihil odio sunt
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+            animi totam aliquid sapiente quam, voluptatibus nam nemo distinctio!
+            Facilis, tempora?
+          </p>
+          <div>
+            <FaFile style={{ fontSize: "2em", textAlign: "center" }} />
+            <p>Attach file</p>
+          </div>
           <label htmlFor="">
+            select type
             <select name="" id="">
               <option value="pdf">pdf</option>
               <option value="png">png</option>
